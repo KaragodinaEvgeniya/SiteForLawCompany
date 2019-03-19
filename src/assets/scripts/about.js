e080f7e96239ebea8931d3e2554184b472bdc7e1
EvgenyaKaragodina <info@avokado24.ru>
2019-03-19 16:59:37 +0500

    first commit

---
+++ src/assets/scripts/about.js
@@ -0,0 +1,7 @@
+import preloader from './modules/preloader';
+import "./modules/map";
+import "./modules/hamburger";
+import "./modules/skills";
+
+document.addEventListener("DOMContentLoaded", preloader);
+
